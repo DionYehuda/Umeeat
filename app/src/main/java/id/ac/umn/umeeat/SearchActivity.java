30a20caf562c2305552132bacc3fc1fedc6d0397
gdcumn <99084286+gdcumn@users.noreply.github.com>
2022-11-09 20:51:14 +0700

    uas

--- app/src/main/java/id/ac/umn/umeeat/SearchActivity.java
+++ app/src/main/java/id/ac/umn/umeeat/SearchActivity.java
@@ -1,5 +1,7 @@
 package id.ac.umn.umeeat;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -7,4 +9,11 @@ public class SearchActivity extends AppCompatActivity  {
 
     private Toolbar toolbar;
     @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.hmToolbar);
+        toolbar.setTitle("Chat");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.black));
+    }
 }
