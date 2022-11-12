e52aaec1b186721a5551cb4475d23e891ab3a504
tesalonikabigail <tesalonika.abigail@student.umn.ac.id>
2022-11-12 23:26:05 +0700

    bagian tesa

--- app/src/main/java/id/ac/umn/umeeat/HomeActivity.java
+++ app/src/main/java/id/ac/umn/umeeat/HomeActivity.java
@@ -3,19 +3,15 @@ package id.ac.umn.umeeat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        toolbar = findViewById(R.id.hmToolbar);
-        toolbar.setTitle("Chat");
-        toolbar.setTitleTextColor(getResources().getColor(R.color.black));
+//        toolbar = findViewById(R.id.hmToolbar);
+//        toolbar.setTitle("Chat");
+//        toolbar.setTitleTextColor(getResources().getColor(R.color.black));
     }
 }
