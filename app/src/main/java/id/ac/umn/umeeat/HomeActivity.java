d9902110ea6687c2ca7717b5545cd91c0937d1b4
Steven Vincent <steven.hendrawan@student.umn.ac.id>
2022-11-13 18:47:04 +0700

    Auto stash before merge of "feature/homeActivity" and "origin/master"
    Layout Home Activity with dummy data, app icon, and some tiny miscellanous content

--- app/src/main/java/id/ac/umn/umeeat/HomeActivity.java
+++ app/src/main/java/id/ac/umn/umeeat/HomeActivity.java
@@ -1,32 +1,52 @@
 package id.ac.umn.umeeat;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final LinkedList<String> friendUName = new LinkedList<>();
+    private static final LinkedList<String> lastChat = new LinkedList<>();
+    private static boolean initRun = true;
     private Toolbar hmToolbar;
     private Button btnChat;
+    private TextView greet;
+    private RecyclerView chatListView;
+    private ChatAdapter chatAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-//        hmToolbar = findViewById(R.id.hmToolbar);
-//        hmToolbar.setTitle("Home");
-//        hmToolbar.setTitleTextColor(getResources().getColor(R.color.black));
-        btnChat = findViewById(R.id.btnChat);
-        btnChat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this, ChatActivity.class));
-            }
-        });
+        hmToolbar = findViewById(R.id.hmToolbar);
+        hmToolbar.setTitle("Home");
+        hmToolbar.setTitleTextColor(getResources().getColor(R.color.black));
+        chatListView =findViewById(R.id.chatrecyclerview);
+
+        if(initRun)
+        {
+            friendUName.add("Ayang");
+            lastChat.add("okee, hari kamis ya");
+            initRun = false;
+        }
+
+        chatAdapter = new ChatAdapter(this, friendUName, lastChat);
+        chatListView.setAdapter(chatAdapter);
+        chatListView.setLayoutManager(new LinearLayoutManager(this));
+
+//        btnChat = findViewById(R.id.btnChat);
+//        btnChat.setOnClickListener(view -> startActivity(new Intent(HomeActivity.this, ChatActivity.class)));
+
+        String userName = getIntent().getStringExtra("MyUsername");
+        greet = findViewById(R.id.greeting);
+        greet.setText("Hey, "+userName+"!\nWho are you eating with today?");
     }
 }
