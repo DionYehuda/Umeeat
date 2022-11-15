50f2343fd3f6cbdfd5238077cee108cc47f203b7
gdcumn <99084286+gdcumn@users.noreply.github.com>
2022-11-15 21:10:21 +0700

    chat + profile orang

---
+++ app/src/main/java/id/ac/umn/umeeat/OtherUserProfileActivity.java
@@ -0,0 +1,60 @@
+package id.ac.umn.umeeat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OtherUserProfileActivity extends AppCompatActivity {
+    TextView uname, ang, jur, gen, desc;
+    String username, angkatan, jurusan, gender, description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_user_profile);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Profile");
+
+        username = "Ayang";
+        angkatan = "2021";
+        jurusan = "Ilmu Komunikasi";
+        gender = "Male";
+        description = "Suka Pedas hehe";
+
+        uname = findViewById(R.id.tvOtherUsername);
+        ang = findViewById(R.id.tvOtherAngkatan);
+        jur = findViewById(R.id.tvOtherJurusan);
+        gen = findViewById(R.id.tvOtherGender);
+        desc = findViewById(R.id.tvOtherDesc);
+
+        uname.setText(username);
+        ang.setText(angkatan);
+        jur.setText(jurusan);
+        gen.setText(gender);
+        desc.setText(description);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
